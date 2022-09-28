3b800078e30fd12b68882efe4a7b1e294e7292fc
Cedrick Monesit <cedrickmonesit@gmail.com>
2022-09-28 09:43:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -321,7 +321,8 @@ app.delete("/favorites/remove-product", authorizeAccessToken, async (request, re
 
 // get user's favorites list
 app.get("/favorites", authorizeAccessToken, async (request, response) => {
-  // Auth0 user sub id used for querying and sending data to database
+  response.status(200).json({ success: true, message: "success" });
+  /* // Auth0 user sub id used for querying and sending data to database
   const subId = request.auth.sub.split("|").pop();
   console.log(subId);
 
@@ -370,7 +371,7 @@ app.get("/favorites", authorizeAccessToken, async (request, response) => {
     .catch((error) => {
       // error message
       return { success: false, error: error, message: "Error" };
-    });
+    });*/
 });
 
 // get all products

9e9aa578025064f5915897e034d5b648d4567e6b
Cedrick Monesit <cedrickmonesit@gmail.com>
2022-09-28 00:32:31 -0400

    Update app.js

--- app.js
+++ app.js
@@ -26,18 +26,23 @@ const Multer = require("multer");
 
 // initialize firestore
 initializeApp({
-  credential: cert({
+  credential: cert(
+    JSON.stringify({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+  ),
+});
+
+// initialize firebase storage
+const storage = new Storage(
+  JSON.stringify({
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
   }),
-});
-
-// initialize firebase storage
-const storage = new Storage({
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
-});
+);
 
 const db = getFirestore();
 

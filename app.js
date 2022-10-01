e4ee9a06290b5a2bdeeaf235571309738c358c98
Cedrick Monesit <cedrickmonesit@gmail.com>
2022-10-01 14:52:45 -0400

    env variables updates
    revoked previous firebase key since it was  accidentally committed

--- app.js
+++ app.js
@@ -27,34 +27,34 @@ const Multer = require("multer");
 // initialize firestore
 initializeApp({
   credential: cert({
-    project_id: "kixs-1d4f3",
-    private_key_id: "6c9829937305027d54eac286a069e46b511410c9",
+    project_id: process.env.PROJECT_ID,
+    private_key_id: process.env.PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
-    client_email: "firebase-adminsdk-2u33w@kixs-1d4f3.iam.gserviceaccount.com",
-    client_id: "100303831849452795178",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2u33w%40kixs-1d4f3.iam.gserviceaccount.com",
+    client_email: process.env.CLIENT_EMAIL,
+    client_id: process.env.CLIENT_ID,
+    auth_uri: process.env.AUTH_URI,
+    token_uri: process.env.TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
   }),
 });
 
 // initialize firebase storage
 const storage = new Storage({
-  project_id: "kixs-1d4f3",
-  private_key_id: "6c9829937305027d54eac286a069e46b511410c9",
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
-  client_email: "firebase-adminsdk-2u33w@kixs-1d4f3.iam.gserviceaccount.com",
-  client_id: "100303831849452795178",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-2u33w%40kixs-1d4f3.iam.gserviceaccount.com",
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
 });
 
 const db = getFirestore();
 
-const bucket = storage.bucket("kixs-1d4f3.appspot.com");
+const bucket = storage.bucket(process.env.BUCKET);
 
 const multer = Multer({
   storage: Multer.memoryStorage(),
